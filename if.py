4d47c55309c440e5b2dc933c468e6d04e59382a4
Nusra Ahmed <147923885+Nueeir@users.noreply.github.com>
2024-12-12 08:49:57 +0300

    Add files via upload
     I learnt conditional statement if-elif-else and used the knowledge to create a program that checks whether a year is a common, leap, and not within the Gregorian calendar.

---
+++ if.py
@@ -0,0 +1,11 @@
+year = int(input("Enter a year: "))
+if year < 1582:
+    print("Not within the Gregorian calendar period.")
+if year % 4 != 0:
+    print("It's a common year.")
+elif year %100 != 0:
+    print("It's a leap year.")
+elif year %400 != 0:
+    print("It's a common year.")
+else:
+    print("It's a leap year.")
